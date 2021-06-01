02121841b947975835c6cb6ec7dbe4693e43bd4a
Anderson Chagas <aachagas@yahoo.com.br>
2021-06-01 16:32:49 -0300

    1.1

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Data;
+using Domain;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando...");
+            var ctx = new ClienteContext();
+
+
+            Cliente cli = new Cliente();
+            cli.id = 1;
+            cli.nome = "Anderson";
+            cli.email = "aachagas@yahoo.com.br";
+            ctx.Clientes.Add(cli);
+            Console.WriteLine("Fim...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Data/ClienteContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Data
+{
+    public class ClienteContext : DbContext
+    {
+
+        public ClienteContext : base("dbClientes"){
+            
+            
+            }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
