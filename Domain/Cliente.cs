48b04a7a64f02a9ec49ba81893f3e76babcb49d4
Anderson Chagas <aachagas@yahoo.com.br>
2021-06-01 15:56:59 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+
+
+
+    }
+}
